847b1d91c03771fdb86a6786725f64a9c96e4f63
ORK_MARK <dravenmark335@gmail.com>
2020-11-09 10:02:56 +0300

    3q (ft_swap) 4 delay

--- D0_03/D0_03.cpp
+++ D0_03/D0_03.cpp
@@ -14,10 +14,19 @@ void ft_ultimate_ft(int********* nbr) {
 	*********nbr = 42;
 }
 
+void ft_swap(int* a, int* b) {
+	int pb = *b;
+	int pa = *a;
+	*a = pb;
+	*b = pa;
+}
+
 int main()
 {
 	int a = 5;
+	int b = 1;
 	//ft_ft();
 	//ft_ultimate_ft();
-	printf("%d\n", a); // p адрес а,  *p вывод числа 5
+	ft_swap(&a, &b);
+	printf("%d %d\n", a, b); // p адрес а,  *p вывод числа 5
 }
